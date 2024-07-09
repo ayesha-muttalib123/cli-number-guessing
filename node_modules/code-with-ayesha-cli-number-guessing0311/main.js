3321167aa0a1acb90aa3e14aceefd67b599a6804
ayesha-muttalib123 <ayeshamutallib007@gmail.com>
2024-07-10 01:32:53 +0500

    number guessing game using cli

---
+++ node_modules/code-with-ayesha-cli-number-guessing0311/main.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// comp will generate random number 
+//user give input for guessing number
+//compare user input with computer generated number
+const random_number = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    { message: "Enter your guess number between 1-10",
+        name: 'userGuessNumber',
+        type: 'number',
+    }
+]);
+console.log(random_number);
+if (answers.userGuessNumber === random_number) {
+    console.log("You won the game");
+}
+else {
+    console.log("You lost the game");
+}
